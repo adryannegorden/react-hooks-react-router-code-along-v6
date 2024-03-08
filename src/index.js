ed42780c2ed7deeac27059ce59042d097f534d7a
adryannegorden <adryannegorden@gmail.com>
2024-03-08 17:55:14 -0500

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
